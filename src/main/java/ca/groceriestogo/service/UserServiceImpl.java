dba2d6212fcf6c12c017a089e847b3a22369ef1f
Daniel Hinbest <daniel.hinbest@dcmail.ca>
2022-02-11 19:10:33 -0500

    User authentication complete. Just needs documentation

--- src/main/java/ca/groceriestogo/service/UserServiceImpl.java
+++ src/main/java/ca/groceriestogo/service/UserServiceImpl.java
@@ -11,22 +11,22 @@ import java.util.HashSet;
 
 @Service
 public class UserServiceImpl implements UserService {
-	@Autowired
-	private UserRepository userRepository;
-	@Autowired
-	private RoleRepository roleRepository;
-	@Autowired
-	private BCryptPasswordEncoder bCryptPasswordEncoder;
-	
-	@Override
-	public void save(User user){
-		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-		user.setRoles(new HashSet<>(roleRepository.findAll()));
-		userRepository.save(user);
-	}
-	
-	@Override
-	public User findByUsername(String username) {
-		return userRepository.findByUsername(username);
-	}
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public void save(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles(new HashSet<>(roleRepository.findAll()));
+        userRepository.save(user);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
 }
