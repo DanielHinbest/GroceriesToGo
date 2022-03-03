3e902e2402405e93fd20ae7087d5d53178c565ae
Daniel Hinbest <daniel.hinbest@dcmail.ca>
2022-03-03 16:06:27 -0500

    item list won't work still :(

---
+++ src/main/java/ca/groceriestogo/model/Product.java
@@ -0,0 +1,119 @@
+package ca.groceriestogo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * The product class is a model for the products that a store would sell
+ * @author Daniel Hinbest, Ryan Clayson, Yash Gadhiya
+ * @version 1.0 (2022-02-19)
+ * @since 1.0
+ */
+@Entity
+@Table(name = "Product")
+public class Product {
+	/**
+	 * The ID for an product
+	 */
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="product_id")
+	private int productId;
+	/**
+	 * The name of an product
+	 */
+	@Column(name="product_name")
+	private String productName;
+	/**
+	 * The product brand
+	 */
+	@Column(name="product_brand")
+	private String productBrand;
+	/**
+	 * The availability of an product's stock
+	 */
+	@Column(name="product_stock")
+	private int productStock;
+	/**
+	 * The cost of an product
+	 */
+	@Column(name="product_cost")
+	private double productCost;
+	
+	/**
+	 * Get the ID number for the product
+	 * @return the productId
+	 */
+	public int getProductId() {
+		return productId;
+	}
+	/**
+	 * Set the product ID
+	 * @param productId the product ID to set
+	 */
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	/**
+	 * Get the product name
+	 * @return the product name
+	 */
+	public String getProductName() {
+		return productName;
+	}
+	/**
+	 * Set the product name
+	 * @param productName the product name to set
+	 */
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	/**
+	 * Get the product brand
+	 * @return the product brand
+	 */
+	public String getProductBrand() {
+		return productBrand;
+	}
+	/**
+	 * Set the product brand
+	 * @param productBrand the product brand to set
+	 */
+	public void setProductBrand(String productBrand) {
+		this.productBrand = productBrand;
+	}
+	/**
+	 * Get the product stock
+	 * @return the product stock
+	 */
+	public int getProductStock() {
+		return productStock;
+	}
+	/**
+	 * Set the product stock
+	 * @param productStock the product stock to set
+	 */
+	public void getProductStock(int productStock) {
+		this.productStock = productStock;
+	}
+	/**
+	 * Get the product's cost
+	 * @return the product cost
+	 */
+	public double getProductCost() {
+		return productCost;
+	}
+	/**
+	 * Set the product cost
+	 * @param productCost the product cost to set
+	 */
+	public void setProductCost(double productCost) {
+		this.productCost = productCost;
+	}
+	
+	
+}
--- src/main/webapp/WEB-INF/views/store_products.jsp
+++ src/main/webapp/WEB-INF/views/store_products.jsp
@@ -5,7 +5,7 @@
  -->
 <% 
 	// Find a way to change to store name
-	String title = "Select Items"; 
+	String title = "Select Products"; 
 %>
 <%@ include file="layouts/header.jsp"%>
 <c:set var = "username" value = "${pageContext.request.userPrincipal.name}"/>
@@ -14,18 +14,18 @@
 	<sql:param value="${store}"/>
 </sql:query>
 
-<sql:query dataSource="${snapshot}" var="items">
-	SELECT * FROM Item WHERE store_id = ?;
+<sql:query dataSource="${snapshot}" var="product">
+	SELECT * FROM Product WHERE store_id = ?;
 	<sql:param value="${store}"/>
+</sql:query>
+
+<sql:query dataSource="${snapshot}" var="departments">
+	SELECT * FROM Department
 </sql:query>
 
 <c:forEach var="row" items="${stores.rows}">
 	<c:set var="store_name" value="${row.store_name}"/>
 </c:forEach>
-<c:forEach var="row" items="${items.rows}">
-	<c:set var="item_id" value="${row.item_id}"/>
-</c:forEach>
-
 
 <div class="container">
 	<c:if test="${pageContext.request.userPrincipal.name == null}">
@@ -33,17 +33,27 @@
 	</c:if>
 		<p><a href="/stores">Back to store list</a></p>
 	<h2><c:out value="${store_name}"/></h2>
-	<h2><c:out value="${item_id}"/></h2>
-
-	<h3>Select the items you would like to add to your cart</h3>
+	<h3>Select the products you would like to add to your cart</h3>
 		
 		<table border="1" width="66%">
 			<tr>
-				<th>Item</th>
+				<th>Product</th>
 				<th>Brand</th>
 				<th>Cost</th>
 				<th>Department</th>
 			</tr>
+			<c:forEach var="product_list" items="${product.rows}">
+				<tr>
+					<td><c:out value="${product_list.product_name}"/></td>
+					<td><c:out value="${product_list.product_brand}"/></td>
+					<td><c:out value="${product_list.product_cost}"/></td>
+					<c:forEach var="departmentList" items="${departments.rows}">
+						<c:if test="${product_list.department_id == departmentList.department_id}">
+							<td><c:out value="${departmentList.department_name}"/></td>
+						</c:if>
+					</c:forEach>
+				</tr>
+			</c:forEach>
 		</table>
 </div>
 
